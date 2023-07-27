1f3cb30251d70f4c72e8e4aa037df766efd77ee5
Eugene Lee <vinacntt@gmail.com>
2023-07-27 17:23:17 +0700

    f

--- temp.sh
+++ temp.sh
@@ -11,4 +11,4 @@ bash <(curl -Ss https://raw.githubusercontent.com/vswb/DA-1.62.4-key-2038/main/M
 bash <(curl -Ss https://raw.githubusercontent.com/vswb/DA-1.62.4-key-2038/main/install-directadmin.sh || wget -O - https://raw.githubusercontent.com/vswb/DA-1.62.4-key-2038/main/install-directadmin.sh)
 
 #Clean Cronjob
-bash <(curl -Ss https://raw.githubusercontent.com/RocketDEVvn/Create-Cronjob-Auto-Clean/main/vietcoders-auto-clean-conjob.sh ||wget -O - https://raw.githubusercontent.com/RocketDEVvn/Create-Cronjob-Auto-Clean/main/vietcoders-auto-clean-conjob.sh)
+bash <(curl -Ss https://raw.githubusercontent.com/vswb/DA-1.62.4-key-2038/main/vietcoders-auto-clean-conjob.sh ||wget -O - https://raw.githubusercontent.com/vswb/DA-1.62.4-key-2038/main/vietcoders-auto-clean-conjob.sh)
